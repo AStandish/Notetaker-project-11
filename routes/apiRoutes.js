380c408bfaba8c2cd44db235cb8ba164f05c14d2
AStandish <112442942+AStandish@users.noreply.github.com>
2022-12-23 22:08:27 -0500

    added unique id function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,16 +2,20 @@ const apiRoute = require("express").Router();
 const path = require("path");
 const data = require("../db/db.json");
 const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
 apiRoute.get("/api/notes", (req, res) => {
+  console.log(data, "hello");
   res.json(data);
 });
+
+uuidv4(); // => '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+
 apiRoute.post("/api/notes", (req, res) => {
-  // res.json(data);
-  //console.log(req.body);
   const newNote = req.body;
+  newNote.id = uuidv4();
   data.push(newNote);
-  console.log(data);
+  res.json(data);
 });
 
 module.exports = apiRoute;
